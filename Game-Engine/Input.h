3e2978af9c96ab223970cb98e38df8b8c742055b
liam.brown-at-866313985001619 <liam.brown@corel.com>
2023-01-23 11:49:23 -0500

    privatized Input contructor

--- Game-Engine/Input.h
+++ Game-Engine/Input.h
@@ -34,6 +34,7 @@ namespace loom
 		};
 
 	private:
+		Input() { };
 		std::vector<std::pair<input, Task>> keys;
 		std::vector<std::pair<input, Task>> mbns;
 		std::unordered_map<uint64_t, Task> inputs;
